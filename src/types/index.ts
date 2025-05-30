592a00fe17d768e663c6efea02a913dd965827df
fudgey792 <shaun_mcfadden@hotmail.co.uk>
2025-05-30 22:29:38 +0100

    Declare strong types for Customer and Note

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export type Customer = {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
+};
+
+export type Note = {
+  id: string;
+  content: string;
+  timestamp: string;
+};

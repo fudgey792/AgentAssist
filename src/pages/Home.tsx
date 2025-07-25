fbef3aeb155d02fbe70c9de20ad60b32bf9b8121
fudgey792 <shaun_mcfadden@hotmail.co.uk>
2025-07-25 22:07:46 +0100

    feat(home): updated home page styling and layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import '../App.css';
 import { Link } from "react-router-dom";
+import './Home.css';
+import { MdSupportAgent } from "react-icons/md";
 
 const Home = () => {
     
     return (
-        <div>
+        <div className="home-container">
+            <div className="home-icon"><MdSupportAgent /></div>
             <h2>Agent Assist Dashboard</h2>
-            <Link to="/dashboard">Dashboard</Link>
+            <p>A lightweight dashboard designed to simulate what a call centre agent might use</p>
+            <Link to="/dashboard" className="home-button">Sign In</Link>
         </div>
     );
 }
